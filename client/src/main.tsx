52788567bf470e54c90b22d10fcdc22d288fe351
Ethan Niser <ethan@niser.com>
2025-12-17 15:58:17 -0800

    kill old

--- client/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { AppWrapper } from "./App";
-import "./styles.css";
-
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-root.render(<AppWrapper />);
